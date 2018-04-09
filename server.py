bfd6a6e855835df744217a495ed1d8146bb0db84
jonhalverson1 <jonhalverson9@gmail.com>
2018-04-09 14:02:37 -0500

    cleaned up a little bit

--- server.py
+++ server.py
@@ -42,55 +42,64 @@ class Server:
                     client_socket, client_address = current_read.accept()
                     client_socket.setblocking(0)
                     self.inputs.append(client_socket)
-                    print("Connections:", len(self.inputs))
 
                 # connection has data for us
                 else:
-                    data = current_read.recv(4096)
+                    uri = self.process_http_header(current_read)
 
-                    # if there is data, send it to all clients in the sender's topic
-                    if data:
-                        json_data = data.decode('utf-8')  # json.loads(data.decode('utf-8'))
+                    # if uri is not None, else 400 Bad Request
+                    if uri is not None:
 
-                        # parse json request, raise 400 error for exception
+                        # send contents of file, if file not found, send 404 error
                         try:
-                            http_as_list = json_data.split("\n")
-                            if len(http_as_list[0].split(" ")) == 3:
-                                uri = "static" + (http_as_list[0].split(" "))[1]
-                            else:
-                                uri = ""
-                            content_type = "Content Type:text/html\r\n\r\n"
-
-                            print("\nData:\n")
-                            print(json_data)
-                            print(http_as_list)
-                            print("URI: ", uri)
-
-                            # send contents of file, if file not found, send 404 error
-                            try:
-                                html_file = open(uri)
-                                html_file_content = html_file.read()
-                                html_file.close()
-                                request_success = "200 OK\r\n"
-
-                            except:
-                                print(uri, "not found")
-                                html_file_content = "<html><body><p>Error 404: File not found</p></body></html>"
-                                request_success = "404 Not Found\r\n"
+                            html_file = open(uri)
+                            html_file_content = html_file.read()
+                            html_file.close()
+                            request_success = "200 OK\r\n"
 
                         except:
-                            print("Bad Request")
+                            print(uri, "not found")
                             html_file_content = "<html><body><p>Error 404: File not found</p></body></html>"
-                            request_success = "400 Bad Request\r\n"
+                            request_success = "404 Not Found\r\n"
 
-                        # craft and send header response
-                        http_response = "HTTP/1.1 " + request_success + content_type
-                        http_response += html_file_content
-                        client_socket.send(http_response.encode())
+                    else:
+                        print("Bad Request")
+                        html_file_content = "<html><body><p>Error 400: Bad Request</p></body></html>"
+                        request_success = "400 Bad Request\r\n"
 
-                        # close connection
-                        self.inputs.remove(current_read)
-                        current_read.close()
+                    content_type = "Content Type:text/html\r\n\r\n"
+
+                    # craft and send header response
+                    http_response = "HTTP/1.1 " + request_success + content_type
+                    http_response += html_file_content
+                    client_socket.send(http_response.encode())
+
+                    # close connection
+                    self.inputs.remove(current_read)
+                    current_read.close()
+
+    # Find the URI
+    def process_http_header(self, socket):
+        data = socket.recv(4096)
+
+        if data:
+            data = data.decode('utf-8')
+            http_as_list = data.split("\n")
+
+            if len(http_as_list[0].split(" ")) == 3:
+                uri = "static" + (http_as_list[0].split(" "))[1]
+
+            else:
+                uri = None
+
+        else:
+            uri = None
+
+        # DEBUGGING
+        print(http_as_list, "\n")
+        print("URI", uri)
+
+        return uri
 
 
 server = Server()
